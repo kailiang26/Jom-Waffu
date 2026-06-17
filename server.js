50c9c4a4263a95825fbbb04d9022585dcae4b244
kailiang26 <kailiang26@gmail.com>
2026-06-17 12:36:26 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,10 +3,8 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from current directory
 app.use(express.static(__dirname));
 
-// Route all requests to index.html
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
